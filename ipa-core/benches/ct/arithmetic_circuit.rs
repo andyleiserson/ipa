3c8b37faf1b37b70f85d07f9fc664d03a892e190
Andy Leiserson <aleiserson@mozilla.com>
2024-01-29 14:31:32 -0800

    Revised arithmetic circuit benchmark (vectorized version)

--- ipa-core/benches/ct/arithmetic_circuit.rs
+++ ipa-core/benches/ct/arithmetic_circuit.rs
@@ -1,35 +1,30 @@
 use criterion::{
-    black_box, criterion_group, criterion_main, measurement::Measurement, BenchmarkGroup,
-    BenchmarkId, Criterion, SamplingMode, Throughput,
+    measurement::Measurement,
+    black_box, criterion_group, criterion_main, BenchmarkGroup, BenchmarkId, Criterion, SamplingMode, Throughput,
 };
 use ipa_core::{
-    ff::{Field, Fp31, Fp32BitPrime},
+    ff::{Fp31, Fp32BitPrime, Field},
     protocol::{basics::SecureMul, context::SemiHonestContext},
     secret_sharing::{replicated::semi_honest::AdditiveShare as Replicated, FieldSimd, IntoShares},
     test_fixture::circuit,
 };
-use rand::distributions::{Distribution, Standard};
+use rand::distributions::{Standard, Distribution};
 use tokio::runtime::{Builder, Runtime};
 
-fn do_benchmark<M, F, const N: usize>(
-    rt: &Runtime,
-    group: &mut BenchmarkGroup<M>,
-    width: u32,
-    depth: u16,
-) where
+fn do_benchmark<M, F, const N: usize>(rt: &Runtime, group: &mut BenchmarkGroup<M>, width: u32, depth: u16, active_work: usize)
+where
     M: Measurement,
-    F: Field + FieldSimd<N>,
-    for<'a> Replicated<F, N>: SecureMul<SemiHonestContext<'a>>,
-    [F; N]: IntoShares<Replicated<F, N>>,
+    F: Field + FieldSimd<N> + IntoShares<Replicated<F>>,
+    for<'a> Replicated<F>: SecureMul<SemiHonestContext<'a>>,
     Standard: Distribution<F>,
 {
     group.throughput(Throughput::Elements((width * depth as u32) as u64));
     group.bench_with_input(
-        BenchmarkId::new("circuit", format!("{width}:{depth}:{}x{}", F::NAME, N)),
+        BenchmarkId::new("circuit", format!("{width}:{depth}:{active_work}:{}x{}", F::NAME, N)),
         &(width, depth),
         |b, &(width, depth)| {
             b.to_async(rt)
-                .iter(|| circuit::arithmetic::<F, N>(black_box(width), black_box(depth)));
+                .iter(|| circuit::arithmetic::<F, N>(black_box(width), black_box(depth), active_work));
         },
     );
 }
@@ -46,14 +41,26 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     group.sample_size(10);
     group.sampling_mode(SamplingMode::Flat);
 
-    do_benchmark::<_, Fp31, 1>(&rt, &mut group, 512_000, 1);
-    do_benchmark::<_, Fp31, 1>(&rt, &mut group, 51_200, 10);
-    do_benchmark::<_, Fp31, 1>(&rt, &mut group, 8_000, 64);
+    // The width parameter (2nd-to-last argument to do_benchmark) must
+    // be a multiple of the vectorization width.
 
-    do_benchmark::<_, Fp32BitPrime, 1>(&rt, &mut group, 25_600, 10);
-    do_benchmark::<_, Fp32BitPrime, 1>(&rt, &mut group, 2_560, 100);
-    do_benchmark::<_, Fp32BitPrime, 32>(&rt, &mut group, 4_000, 64);
-    do_benchmark::<_, Fp32BitPrime, 32>(&rt, &mut group, 250, 1_024);
+    // These total 262,144 elements.
+    do_benchmark::<_, Fp32BitPrime, 32>(&rt, &mut group, 16_384,    16,    8);
+    do_benchmark::<_, Fp32BitPrime, 32>(&rt, &mut group, 16_384,    16,   32);
+    do_benchmark::<_, Fp32BitPrime, 32>(&rt, &mut group, 16_384,    16,  128);
+    do_benchmark::<_, Fp32BitPrime, 32>(&rt, &mut group, 16_384,    16,  512);
+    do_benchmark::<_, Fp32BitPrime, 32>(&rt, &mut group,  2_048,   128,    8);
+    do_benchmark::<_, Fp32BitPrime, 32>(&rt, &mut group,  2_048,   128,   32);
+    do_benchmark::<_, Fp32BitPrime, 32>(&rt, &mut group,  2_048,   128,  128);
+    do_benchmark::<_, Fp32BitPrime, 32>(&rt, &mut group,  2_048,   128,  512);
+
+    // These total 1,048,576 elements.
+    do_benchmark::<_, Fp32BitPrime,  32>(&rt, &mut group,  1_024, 1_024,   8);
+    do_benchmark::<_, Fp32BitPrime,  32>(&rt, &mut group,  1_024, 1_024,  32);
+    do_benchmark::<_, Fp32BitPrime,  32>(&rt, &mut group,  1_024, 1_024, 128);
+    do_benchmark::<_, Fp32BitPrime, 256>(&rt, &mut group,  4_096,   256,   8);
+    do_benchmark::<_, Fp32BitPrime, 256>(&rt, &mut group,  4_096,   256,  32);
+    do_benchmark::<_, Fp32BitPrime, 256>(&rt, &mut group,  4_096,   256, 128);
 }
 
 criterion_group!(benches, criterion_benchmark);
--- ipa-core/src/test_fixture/circuit.rs
+++ ipa-core/src/test_fixture/circuit.rs
@@ -1,89 +1,123 @@
-use futures_util::future::join_all;
-use rand::distributions::{Distribution, Standard};
+use std::num::NonZeroUsize;
 
-use super::join3v;
+use futures::{stream, StreamExt};
+use futures_util::future::join3;
+use rand::distributions::{Standard, Distribution};
+
 use crate::{
     ff::Field,
-    helpers::TotalRecords,
+    helpers::GatewayConfig,
     protocol::{
         basics::SecureMul,
         context::{Context, SemiHonestContext},
         RecordId,
     },
     rand::thread_rng,
-    secret_sharing::{replicated::semi_honest::AdditiveShare as Replicated, FieldSimd, IntoShares},
-    test_fixture::{narrow_contexts, ReconstructArr, TestWorld},
+    secret_sharing::{replicated::semi_honest::AdditiveShare as Replicated, IntoShares, FieldSimd},
+    test_fixture::{TestWorld, ReconstructArr, TestWorldConfig}, seq_join::seq_join,
 };
+
+struct Inputs<F: Field + FieldSimd<N>, const N: usize> {
+    a: Replicated<F, N>,
+    b: Vec<Replicated<F, N>>,
+}
+
+impl<F: Field + FieldSimd<N>, const N: usize> Inputs<F, N> {
+    fn new(a: Replicated<F, N>, b: Vec<Replicated<F, N>>) -> Self {
+        Self { a, b }
+    }
+}
 
 /// Creates an arithmetic circuit with the given width and depth.
 ///
 /// # Panics
 /// panics when circuits did not produce the expected value.
-pub async fn arithmetic<F, const N: usize>(width: u32, depth: u16)
+pub async fn arithmetic<F, const N: usize>(width: u32, depth: u16, active_work: usize)
 where
     F: Field + FieldSimd<N>,
     for<'a> Replicated<F, N>: SecureMul<SemiHonestContext<'a>>,
     [F; N]: IntoShares<Replicated<F, N>>,
     Standard: Distribution<F>,
 {
-    let world = TestWorld::default();
+    // The default GatewayConfig is optimized for performance. The default TestWorldConfig uses a
+    // modified GatewayConfig that is optimized for unit tests.
+    let mut config = TestWorldConfig::default();
+    config.gateway_config = GatewayConfig::new(active_work);
+    let world = TestWorld::new_with(config);
+
     // Re-use contexts for the entire execution because record identifiers are contiguous.
     let contexts = world.contexts();
 
-    let mut multiplications = Vec::new();
-    for record in 0..width {
-        let circuit_result = circuit(&contexts, RecordId::from(record), depth);
-        multiplications.push(circuit_result);
+    let mut inp0 = Vec::with_capacity(width as usize / N);
+    let mut inp1 = Vec::with_capacity(width as usize / N);
+    let mut inp2 = Vec::with_capacity(width as usize / N);
+    for _ in 0..(width / (N as u32)) {
+        let [a0, a1, a2] = [F::ONE; N].share_with(&mut thread_rng());
+        let mut b0 = Vec::with_capacity(depth as usize);
+        let mut b1 = Vec::with_capacity(depth as usize);
+        let mut b2 = Vec::with_capacity(depth as usize);
+        for _ in 0..(depth as usize) {
+            let [s0, s1, s2] = [F::ONE; N].share_with(&mut thread_rng());
+            b0.push(s0);
+            b1.push(s1);
+            b2.push(s2);
+        }
+        inp0.push(Inputs::new(a0, b0));
+        inp1.push(Inputs::new(a1, b1));
+        inp2.push(Inputs::new(a2, b2));
     }
 
-    #[allow(clippy::disallowed_methods)] // Just for testing purposes.
-    let results = join_all(multiplications).await;
-    let mut sum = [0u128; N];
-    for line in results {
-        for (this_sum, this_value) in sum.iter_mut().zip(line.reconstruct_arr()) {
-            *this_sum += this_value.as_u128();
+    let [fut0, fut1, fut2] = match contexts.into_iter().zip([inp0, inp1, inp2])
+        .map(|(ctx, col_data)| {
+            let ctx = ctx.set_total_records(width as usize / N);
+            seq_join(
+                NonZeroUsize::new(active_work).unwrap(),
+                stream::iter((0..(width / (N as u32))).zip(col_data))
+                    .map(move |(record, Inputs { a, b })| {
+                        circuit(ctx.clone(), RecordId::from(record), depth, a, b)
+                    })
+            )
+            .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>()
+        .try_into()
+    {
+        Ok(futs) => futs,
+        Err(_) => panic!("infallible try_into array")
+    };
+
+    let (res0, res1, res2) = join3(fut0, fut1, fut2).await;
+
+    let mut sum = 0;
+    for line in res0.into_iter().zip(res1).zip(res2) {
+        let ((s0, s1), s2) = line;
+        for col_sum in [s0, s1, s2].reconstruct_arr() {
+            sum += col_sum.as_u128();
         }
     }
 
-    assert_eq!(sum, [u128::from(width); N]);
+    assert_eq!(sum, u128::from(width));
 }
 
 async fn circuit<'a, F, const N: usize>(
-    top_ctx: &[SemiHonestContext<'a>; 3],
+    ctx: SemiHonestContext<'a>,
     record_id: RecordId,
     depth: u16,
-) -> [Replicated<F, N>; 3]
+    mut a: Replicated<F, N>,
+    b: Vec<Replicated<F, N>>,
+) -> Replicated<F, N>
 where
     F: Field + FieldSimd<N>,
     Replicated<F, N>: SecureMul<SemiHonestContext<'a>>,
-    [F; N]: IntoShares<Replicated<F, N>>,
 {
-    assert_eq!(
-        depth % u16::try_from(N).unwrap(),
-        0,
-        "depth must be a multiple of vectorization factor"
-    );
 
-    let mut a = [F::ONE; N].share_with(&mut thread_rng());
-
-    for stripe in 0..(depth / u16::try_from(N).unwrap()) {
-        let b = [F::ONE; N].share_with(&mut thread_rng());
-        let stripe_ctx = narrow_contexts(top_ctx, &format!("s{stripe}"));
-        a = async move {
-            let mut coll = Vec::new();
-            for (i, ctx) in stripe_ctx.iter().enumerate() {
-                let mul = a[i].multiply(
-                    &b[i],
-                    ctx.narrow("mult")
-                        .set_total_records(TotalRecords::Indeterminate),
-                    record_id,
-                );
-                coll.push(mul);
-            }
-
-            join3v(coll).await
-        }
-        .await;
+    for stripe in 0..(depth as usize) {
+        let stripe_ctx = ctx.narrow(&format!("s{stripe}"));
+        a = a.multiply(
+            &b[stripe],
+            stripe_ctx,
+            record_id,
+        ).await.unwrap();
     }
 
     a
