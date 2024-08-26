083081b8720384cfc1b6880d879a16be5c8deb24
Andy Leiserson <aleiserson@mozilla.com>
2024-08-26 16:05:55 -0700

    Fix child of dp noisegen step

--- ipa-core/src/protocol/dp/step.rs
+++ ipa-core/src/protocol/dp/step.rs
@@ -2,7 +2,7 @@ use ipa_step_derive::CompactStep;
 
 #[derive(CompactStep)]
 pub(crate) enum DPStep {
-    #[step(child = crate::protocol::ipa_prf::aggregation::step::AggregateValuesStep)]
+    #[step(child = crate::protocol::ipa_prf::aggregation::step::AggregateChunkStep)]
     NoiseGen,
     #[step(child = crate::protocol::boolean::step::ThirtyTwoBitStep)]
     ApplyNoise,
